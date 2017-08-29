623ff59560c16a7706a123e4dab653b723b4fbf8
Fabio Ono Tavoloni <fabioono25@gmail.com>
2017-08-29 11:49:48 -0300

    asdasd

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApiTest.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2asas" };
         }
 
         // GET api/values/5
